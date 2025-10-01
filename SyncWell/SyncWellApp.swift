021d49535140d4ded83c0f718a69bb0535caac4a
mohamed <feituri4456@gmail.com>
2025-10-01 12:16:28 -0600

    Initial Commit

---
+++ SyncWell/SyncWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncWellApp.swift
+//  SyncWell
+//
+//  Created by Mohamed Feituri on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SyncWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
